85bf44cff27ff039dfd434f5d64cc7044af2206f
Geonwoo Kim <gwkim973@gmail.com>
2023-06-03 20:28:24 -0700

    added question.py and session.py, and also updated mumei.sh

--- src/mumei.sh
+++ src/mumei.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+if [$1 == 'start']
+then
+    cd 
+    cd Documents/Programming/Github/Assistant/src/
+    python3 session.py
+else
+    cd 
+    cd Documents/Programming/Github/Assistant/src/
+    python3 question.py
+fi
