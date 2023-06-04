ba82257284f3071624cebe3334ed1ccb257cd094
Geonwoo Kim <gwkim973@gmail.com>
2023-06-03 22:47:21 -0700

    gave ai assistant a personality

--- src/session.py
+++ src/session.py
@@ -1,21 +1,25 @@
 import openai
 
 f = open("./assets/API_KEY.txt", "r")
-
 openai.api_base = 'https://api.pawan.krd/v1'
 openai.api_key = f.read()
-
 f.close()
 
-def ask(question) :
+f = open("./assets/PERSONALITY.txt", "r")
+messages = [
+    {"role":"system", "content":f.read()}
+]
+f.close()
+
+def ask(question):
+    messages.append({"role":"user", "content":question})
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": question}
-        ]
+        messages=messages
     )
-
-    print(completion.choices[0].message.content)
+    chat_response = completion.choices[0].message.content
+    print(chat_response)
+    messages.append({"role":"assistant", "content":chat_response})
 
 
 while True:
