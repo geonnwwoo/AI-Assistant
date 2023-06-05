28b5cc16e59fc2b0eef50fe023424a7e890af2a7
Geonwoo Kim <gwkim973@gmail.com>
2023-06-04 17:56:00 -0700

    script current director variable

--- src/ask.sh
+++ src/ask.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 if [ "$1" = "start" ]; then
-    cd 
-    cd Documents/Programming/Github/Assistant/src/
+    cd "$script_dir"
     python3 session.py
 else
-    cd 
-    cd Documents/Programming/Github/Assistant/src/
+    cd "$script_dir"
     echo "$1" > ./assets/question.txt
     python3 question.py
 fi
