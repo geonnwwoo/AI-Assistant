503102e5e011b05e255ad9727888ec671381cb4d
Geonwoo Kim <gwkim973@gmail.com>
2023-06-04 18:06:07 -0700

    added set-personality and set-apikey commands

--- src/ask.sh
+++ src/ask.sh
@@ -5,6 +5,12 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [ "$1" = "start" ]; then
     cd "$script_dir"
     python3 session.py
+elif [ "$1" = "set-personality" ]; then
+    cd "$script_dir"
+    echo "$2" > ./assets/PERSONALITY.txt
+elif [ "$1" = "set-apikey" ]; then
+    cd "$script_dir"
+    echo "$2" > ./assets/API_KEY.txt
 else
     cd "$script_dir"
     echo "$1" > ./assets/question.txt
